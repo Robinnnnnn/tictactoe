1726d9cdab6e6c52a7c2ae8adae58cbf3e46b7f3
Your NameRobin <rlifshitz19@gmail.com>
2020-01-16 15:13:37 -0700

    module working

--- script.js
+++ script.js
@@ -2,41 +2,11 @@
 const Gameboard = (function() {
 
     return {
-        boardArr: ["1", "x", "x", "x", "x", "x", "x", "x", "x"],
+        boardArr: ["", "", "", "", "", "", "", "", ""],
     }
 })();
 
-const GameLogic = (function() {
-    //controlls users inputs and relays info to Gameboard
-    //Doesnt allow moves to be played in the same spot
 
-    let moveTracker = 1;
-    const identifySpot = Array.from(document.getElementsByClassName('spots'));
-
-    const rulesOne = () => {
-        for (let i = 0; i < identifySpot.length; i++) {
-            identifySpot[i].setAttribute('id', 'spot' + i);
-            identifySpot[i].addEventListener('click', function(event) {
-                if (moveTracker % 2 == 0) {
-                    event.target.textContent = player1.playerMark;
-                    moveTracker++;
-                } else {
-                    event.target.textContent = player2.playerMark;
-                    moveTracker++;
-                }
-            })
-        };
-    }
-
-    return {
-
-        invokeRuleOne: function() {
-            rulesOne()
-        }
-
-    }
-
-})();
 
 //Factory Functions
 const PlayerFactory = (player, mark) => {
@@ -67,25 +37,35 @@ function render() {
 
 }
 
-// function rulesOne() {
-
-//     let moveTracker = 1;
-//     let identifySpot = Array.from(document.getElementsByClassName('spots'));
-
-//     for (let i = 0; i < identifySpot.length; i++) {
-//         identifySpot[i].setAttribute('id', 'spot' + i);
-//         identifySpot[i].addEventListener('click', function(event) {
-//             if (moveTracker % 2 == 0) {
-//                 event.target.textContent = player1.playerMark;
-//                 moveTracker++;
-//             } else {
-//                 event.target.textContent = player2.playerMark;
-//                 moveTracker++;
-//             }
-//         })
-//     };
-// }
-
 render();
-//rulesOne();
-GameLogic.invokeRuleOne();
+
+const GameLogic = (function() {
+    //controlls users inputs and relays info to Gameboard
+    //Doesnt allow moves to be played in the same spot
+
+    let moveTracker = 1;
+    const identifySpot = Array.from(document.getElementsByClassName('spots'));
+
+
+    return {
+
+        rulesOne: function() {
+
+            for (let i = 0; i < identifySpot.length; i++) {
+                identifySpot[i].setAttribute('id', 'spot' + i);
+                identifySpot[i].addEventListener('click', function(event) {
+                    if (moveTracker % 2 == 0) {
+                        event.target.textContent = player1.playerMark;
+                        moveTracker++;
+                    } else {
+                        event.target.textContent = player2.playerMark;
+                        moveTracker++;
+                    }
+                })
+            };
+        }
+    }
+
+})();
+
+GameLogic.rulesOne();

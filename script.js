ce4fcb4b2581bde5e843407bc8aade035968edc1
Your NameRobin <rlifshitz19@gmail.com>
2020-01-17 19:51:09 -0700

    Cant make move where there is already a move, works

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //Module
-const Gameboard = (function() {
+const Gameboard = (function () {
+
+    let board = ["", "", "", "", "", "", "", "", ""];
 
     return {
-        boardArr: ["", "", "", "", "", "", "", "", ""],
+        boardArr: board,
     }
 })();
 
@@ -39,33 +41,56 @@ function render() {
 
 render();
 
-const GameLogic = (function() {
+//Module IIFE
+const GameLogic = (function () {
     //controlls users inputs and relays info to Gameboard
     //Doesnt allow moves to be played in the same spot
 
     let moveTracker = 1;
     const identifySpot = Array.from(document.getElementsByClassName('spots'));
 
+    const ruleOne = () => {
+        for (let i = 0; i < identifySpot.length; i++) {
+            identifySpot[i].setAttribute('id', 'spot' + i);
+            identifySpot[i].addEventListener('click', function (event) {
+                if (moveTracker % 2 == 0 && event.target.textContent == "") {
+                    event.target.textContent = player1.playerMark;
+                    moveTracker++;
+                } else if (moveTracker % 2 == 1 && event.target.textContent == "") {
+                    event.target.textContent = player2.playerMark;
+                    moveTracker++;
+                } else {
+                    //Will add functionality to blink red to show cant make that move
+                    console.log("ERROR Move Here Alread")
+                }
+            })
+        };
+    }
+
+    const ruleTwo = () => {
+        let spots = Array.from(document.getElementsByClassName('spots'));
+        for (let i = 0; i < spots.length; i++) {
+            Gameboard.boardArr[i] = spots[i].textContent;
+        }
+    }
+
+    const ruleThree = () => {
+        //How to win logic goes here
+
+    }
 
     return {
+        invokeRuleOne: function () {
+            ruleOne();
+        },
 
-        rulesOne: function() {
+        invokeRuleTwo: function () {
+            ruleTwo();
+        },
 
-            for (let i = 0; i < identifySpot.length; i++) {
-                identifySpot[i].setAttribute('id', 'spot' + i);
-                identifySpot[i].addEventListener('click', function(event) {
-                    if (moveTracker % 2 == 0) {
-                        event.target.textContent = player1.playerMark;
-                        moveTracker++;
-                    } else {
-                        event.target.textContent = player2.playerMark;
-                        moveTracker++;
-                    }
-                })
-            };
-        }
     }
 
 })();
 
-GameLogic.rulesOne();
+GameLogic.invokeRuleOne();
+GameLogic.invokeRuleTwo();
